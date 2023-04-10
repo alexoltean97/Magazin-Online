539df479c2b8434d245703374799cd4f6cfc3549
alexoltean97 <alex.oltean97@gmail.com>
2023-04-10 11:40:38 +0300

    Pagination

--- backend/src/store/mutations.js
+++ backend/src/store/mutations.js
@@ -1,18 +1,27 @@
-export function setUser(state, user){
-    state.user.data = user
+export function setUser(state, user) {
+    state.user.data = user;
 }
 
-export function setToken(state,token){
-    state.user.token = token
-    if(token){
-        sessionStorage.setItem('TOKEN', token)
+export function setToken(state, token) {
+    state.user.token = token;
+    if (token) {
+        sessionStorage.setItem('TOKEN', token);
     } else {
-        sessionStorage.removeItem('TOKEN')
+        sessionStorage.removeItem('TOKEN');
     }
 }
 
-export function setProducts(state, [loading,response = {}]){
+export function setProducts(state, [loading, response = null]) {
+    if (response) {
+        state.products = {
+            data: response.data,
+            links: response.meta.links,
+            total: response.meta.total,
+            limit: response.meta.per_page,
+            from: response.meta.from,
+            to: response.meta.to,
+            page: response.meta.current_page
+        };
+    }
     state.products.loading = loading;
-    state.products.data = response.data;
-
 }
--- backend/src/views/Products.vue
+++ backend/src/views/Products.vue
@@ -67,6 +67,38 @@
                     </tr>
                 </tbody>
             </table>
+
+            <div class="flex justify-between items-center mt-5">
+                <span> Showing from {{ products.from }} to {{ products.to }} </span>
+
+                <nav
+                    v-if="products.total > products.limit"
+                    class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
+                    aria-label="Pagination"
+                >
+                    <a
+                        v-if="products && products.links"
+                        v-for="(link, i) of products.links"
+                        :key="i"
+                        :disabled="!link.url"
+                        href="#"
+                        @click.prevent="getForPage($event, link)"
+                        aria-current="page"
+                        class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+                        :class="[
+                            link.active
+                                ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                                : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+
+                            i === 0 ? 'rounded-l-md' : '',
+                            i === products.links.length - 1 ? 'rounded-l-md' : '',
+                            !link.url ? 'bg-gray-100 text-gray 700' : ''
+                        ]"
+                        v-html="link.label"
+                    >
+                    </a>
+                </nav>
+            </div>
         </template>
     </div>
 </template>
@@ -75,27 +107,38 @@
 import Spinner from '../components/core/Spinner.vue';
 import { computed, ref, onMounted } from 'vue';
 import store from '../store/index.js';
+import { PRODUCTS_PER_PAGE } from '../constants.js';
 
 export default {
     name: 'Products',
     components: { Spinner },
 
     setup() {
-        const perPage = ref(10);
+        const perPage = ref(PRODUCTS_PER_PAGE);
         const search = ref('');
         const products = computed(() => store.state.products);
 
-        function getProducts() {
-            store.dispatch('getProducts');
+        function getProducts(url = null,search = '', perPage = 10) {
+            store.dispatch('getProducts', {
+                url,
+                search: search.value,
+                perPage: perPage.value
+            });
         }
 
-        onMounted( () =>{
-           getProducts()
-        })
+        function getForPage(event, link) {
+            if (!link.url || link.active) {
+                return;
+            }
+            getProducts(link.url);
+        }
 
-        return { perPage, search, products, getProducts };
-    },
+        onMounted(() => {
+            getProducts();
+        });
 
+        return { perPage, search, products, getProducts, getForPage };
+    }
 };
 </script>
 
